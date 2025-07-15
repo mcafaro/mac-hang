a93553906b3b44800bfd0a6d1d56403460f863d2
Mark Cafaro <mcafaro@mathworks.com>
2025-07-15 14:06:07 -0400

    Refactor testRunExample method to enable execution and teardown for example tests

--- tinstall.m
+++ tinstall.m
@@ -27,18 +27,19 @@ classdef tinstall < matlab.unittest.TestCase
             testCase.verifyThat(logical(status), IsFalse, Diagnostic.join([restricted ' should not checkout'], msg));
         end
 
-        % function testRunExample(testCase, example)
-        %     testCase.verifyTrue(isInstalled(example), 'Product not installed');
+        function testRunExample(testCase, example)
+            testCase.verifyTrue(isInstalled(example), 'Product not installed');
 
-        %     startingFigs = findall(groot, 'Type','figure');
-        %     testCase.addTeardown(@() close(setdiff(findall(groot, 'Type','figure'), startingFigs)));
+            disp(['Running example for ' example]);
+            startingFigs = findall(groot, 'Type','figure');
+            testCase.addTeardown(@() close(setdiff(findall(groot, 'Type','figure'), startingFigs)));
 
-        %     [log, ex] = evalc('runExample(fullfile(''examples'',example));');
-        %     if ~isempty(ex)
-        %         disp(log);
-        %         rethrow(ex);
-        %     end
-        % end
+            [log, ex] = evalc('runExample(fullfile(''examples'',example));');
+            if ~isempty(ex)
+                disp(log);
+                rethrow(ex);
+            end
+        end
     end
 
 end
